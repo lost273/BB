03000b657d06aab10d61d96afef9fead29f325ff
lost273 <dnk273@gmail.com>
2017-12-17 16:33:21 +0300

    17-12-2017

--- src/bbpack/BeatBox.java
+++ src/bbpack/BeatBox.java
@@ -7,6 +7,9 @@ import javax.swing.*;
 import javax.sound.midi.*;
 import java.util.*;
 import java.awt.event.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 
 public class BeatBox {
     JPanel mainPanel;
@@ -162,5 +165,23 @@ public class BeatBox {
             e.printStackTrace();
         }
         return event;
+    }
+    public class MySendListener implements ActionListener{
+        public void actionPerformed(ActionEvent a){
+            boolean[] checkboxState = new boolean[256];
+            for(int i = 0; i < 256; i++){
+                JCheckBox check = (JCheckBox) checkboxList.get(i);
+                if(check.isSelected()){
+                    checkboxState[i] = true;
+                }
+            }
+            try{
+                FileOutputStream fileStream = new FileOutputStream(new File("Checkbox.ser"));
+                ObjectOutputStream os = new ObjectOutputStream(fileStream);
+                os.writeObject(checkboxState);
+            } catch(Exception ex){
+                ex.printStackTrace();
+            }
+        }
     }
 }
